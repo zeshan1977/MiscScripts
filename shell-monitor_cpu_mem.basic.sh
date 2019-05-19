92411a8843512855638a1ad524aaef0b817fdeed
khan.mza@gmail.com <khan.mza@gmail.com>
2019-05-19 16:19:05 -0400

    took out extra space between the date and timestamp

--- shell-monitor_cpu_mem.basic.sh
+++ shell-monitor_cpu_mem.basic.sh
@@ -12,5 +12,5 @@ sleep 2;
 #DATE=`date`
 #DATE2=`echo $DATE | sed 's/,/|/g'`
 ps axo ruser,%cpu,%mem,pid,command|sort -nr |grep $1
-ps axo ruser,%cpu,%mem,pid,command|sort -nr |grep $1 |awk -v DATE="$(date +"%Y-%m-%d %r")" '{s+=$2;m+=$3} END {printf "DateTime=>%s,CPU=>%.0f,MEM=>%.0f\n",DATE,s,m}'
+ps axo ruser,%cpu,%mem,pid,command|sort -nr |grep $1 |awk -v DATE="$(date +"%Y-%m-%d-%r")" '{s+=$2;m+=$3} END {printf "DateTime=>%s,CPU=>%.0f,MEM=>%.0f\n",DATE,s,m}'
 done
